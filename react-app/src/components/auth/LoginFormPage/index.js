b12cff406d75f7075dec919cc82a1eeab6d44ea1
Mark Vasquez <markjasonvasquez1@gmail.com>
2021-08-16 15:13:49 -0700

    Add green login button

--- react-app/src/components/auth/LoginFormPage/index.js
+++ react-app/src/components/auth/LoginFormPage/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { login } from "../../../store/session";
@@ -33,35 +34,60 @@ const LoginFormPage = () => {
 	}
 
 	return (
-		<form onSubmit={onLogin}>
-			<div>
-				{errors.map((error, ind) => (
-					<div key={ind}>{error}</div>
-				))}
-			</div>
+		<>
 			<AuthHeader />
-			<div className={styles.email_test}>
-				<label htmlFor="email">Email</label>
-				<input
-					name="email"
-					type="text"
-					placeholder="Email"
-					value={email}
-					onChange={updateEmail}
-				/>
+			<div className={styles.page_container}>
+				<div className={styles.middle_container}>
+					<div className={styles.welcome_block}>
+						<h1 className={styles.welcome}>Welcome to FORMe!</h1>
+						<p className={styles.create_login}>
+							Create an account or log in
+						</p>
+					</div>
+					<div className={styles.form_container}>
+						<div className={styles.form_block}>
+							<form className={styles.form} onSubmit={onLogin}>
+								<div>
+									{errors.map((error, ind) => (
+										<div key={ind}>{error}</div>
+									))}
+								</div>
+								<label htmlFor="email"></label>
+								<input
+									className={styles.email}
+									name="email"
+									type="text"
+									placeholder="EMAIL ADDRESS"
+									value={email}
+									onChange={updateEmail}
+								/>
+								<div>
+									<label htmlFor="password"></label>
+									<input
+										className={styles.password}
+										name="password"
+										type="password"
+										placeholder="PASSWORD"
+										value={password}
+										onChange={updatePassword}
+									/>
+									<button
+										className={styles.submit_button}
+										type="submit">
+										LOGIN
+									</button>
+									<p className={styles.first_time_message}>
+										First time on FORMe?
+									</p>
+									<Link></Link>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div className={styles.footer_container}>yayaya</div>
 			</div>
-			<div>
-				<label htmlFor="password">Password</label>
-				<input
-					name="password"
-					type="password"
-					placeholder="Password"
-					value={password}
-					onChange={updatePassword}
-				/>
-				<button type="submit">Login</button>
-			</div>
-		</form>
+		</>
 	);
 };
 
