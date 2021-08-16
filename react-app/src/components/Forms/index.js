f969f980c74474dd624e7e32f9ae4e28d6750284
Dechon Ryan <dechon@tuta.io>
2021-08-16 10:31:36 -0700

    Building Forms store and component

--- react-app/src/components/Forms/index.js
+++ react-app/src/components/Forms/index.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import getForms from '../../store/forms.js'
 
 function Forms() {
+    const dispatch = useDispatch()
+    const forms = Array.from(useSelector(state => state.forms))
+    useEffect(() => {
+        dispatch(getForms())
+    }, [dispatch])
+
     return (
-        <h1>Build Form Test</h1>
+        <>
+            <h1>Build Form Test</h1>
+            <p>{forms}</p>
+        </>
     )
 }
 
