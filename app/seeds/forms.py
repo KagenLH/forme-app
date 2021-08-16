a9231510c5ebf75d49b395a0b7eeedc6929e05d7
Dechon Ryan <dechon@tuta.io>
2021-08-16 13:58:27 -0700

    Create and import seed functions for Forms table

--- app/seeds/forms.py
+++ app/seeds/forms.py
@@ -4,7 +4,7 @@ from app.models import db, Form
 def seed_forms():
     test = Form(
         title='Test Form',
-        owner_id=1,
+        owner_id='1',
         description='This is a test form.'
         # label_align=None
         # description_align=None
@@ -12,4 +12,9 @@ def seed_forms():
     )
 
     db.session.add(test)
+    db.session.commit()
+
+
+def undo_forms():
+    db.session.execute('TRUNCATE forms RESTART IDENTITY CASCADE;')
     db.session.commit()
