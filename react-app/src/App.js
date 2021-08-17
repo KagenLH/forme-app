e050f40be673bed151140f217aae6804a999aad1
Dechon Ryan <dechon@tuta.io>
2021-08-17 08:27:14 -0700

    Fix homepage rendering

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -43,9 +43,6 @@ function App() {
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
         <Route path="/forms" exact={true}>
           <BuildForm />
         </Route>
