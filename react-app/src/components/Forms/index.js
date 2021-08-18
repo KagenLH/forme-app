ef24ce33dbd1985c95d53a9c6a0e22a69ae2be59
NoahNim <nmedoff123@gmail.com>
2021-08-18 08:49:33 -0700

    trying to figure out why delete function not triggering re-render

--- react-app/src/components/Forms/index.js
+++ react-app/src/components/Forms/index.js
@@ -15,21 +15,27 @@ function Forms() {
     // console.log('inside forms object', forms?.forms)
 
     // TODO: make it so forms delete immediately without a refresh
-    const handleDeleteForm = async (formId) => {
+    const handleDeleteForm = async (formId, event) => {
         await dispatch(deleteForm(formId))
         await dispatch(getForms())
+
+        delete event.target.id
+        // await dispatch(deleteForm(formData))
+
+
+        // await dispatch(getForms())
     }
 
     //! testing only
-    // const formData = {
-    //     id: 9,
-    //     owner_id: 1,
-    //     title: "Testing form creation",
-    //     description: "This form tests form creation.",
-    //     title_align: null,
-    //     label_align: null,
-    //     description_align: null
-    // }
+    const formData = {
+        id: 9,
+        owner_id: 1,
+        title: "Testing form creation",
+        description: "This form tests form creation.",
+        title_align: null,
+        label_align: null,
+        description_align: null
+    }
 
     // new form submission -- this function works properly
     //! move to another component?
@@ -39,7 +45,7 @@ function Forms() {
     // }
 
 
-    let finalForm = forms?.forms
+    // let finalForm = forms?.forms
     return (
         <div className='form-manager-container'>
             <div className='form-manager-page-header'>
@@ -64,12 +70,12 @@ function Forms() {
                             </thead>
                             <>
                             <tbody>
-                                {finalForm?.map(form => {
+                                    {forms?.forms?.map(form => {
                                     return (
                                         <>
                                             <tr className='form-table-rows'>
                                                 <td className='form-table-data' key={form?.id}>{form?.title}</td>
-                                                <td className='delete-buttons' onClick={() => handleDeleteForm(form?.id)}> - Delete Form</td>
+                                                <td className='delete-buttons' onClick={(e) => handleDeleteForm(form?.id, e)}> - Delete Form</td>
                                             </tr>
                                         </>
                                     )
