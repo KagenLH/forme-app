ead662711375868e69976abbde5822bf7cc047a9
Dechon Ryan <dechon@tuta.io>
2021-08-19 15:42:01 -0700

    Add simple styling to form sharing modal. Include copy button in modal.

--- react-app/src/components/Forms/ShareFormLink.js
+++ react-app/src/components/Forms/ShareFormLink.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ShareFormLink({ formUrl }) {
+    const [showCopyBtn, setShowCopyBtn] = useState(true)
+
+    const copyText = () => {
+        document.querySelector('.form-link-box').select()
+        document.execCommand('copy')
+        // document.getElementById('copy-btn').innerHTML = '✅'
+        setShowCopyBtn(false)
+    }
+
     return (
-        <>
-            <label>Copy this link:</label>
-            <input type='text' value={formUrl}></input>
-        </>
+        <div className='form-link-container'>
+            <label className='form-link-label'>Copy this link: </label>
+            <input className='form-link-box' type='text' value={formUrl} />
+            {showCopyBtn ? (
+                <i id='copy-btn' title='Copy' onClick={() => copyText()} className="fa fa-clipboard copy-btn" aria-hidden="true" />
+            ) : (
+                <span className='copy-btn copy-confirm'>Copied! ✅</span>
+            )
+            }
+        </div>
     )
 }
 
