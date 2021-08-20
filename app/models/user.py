1118f28f16b53001f3ed7c32dd28a960aa2cf8ba
Kagen Hearn <kl.hearn40@gmail.com>
2021-08-20 13:55:07 -0400

    Fixed syntax error

--- app/models/user.py
+++ app/models/user.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     forms = db.relationship('Form', backref='users', lazy=True)
-    username = db.Column(db.String(40), nullable=False, unFalseique=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
