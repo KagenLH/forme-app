a6f8bcaa31c55d40565c8e3d541bd94e9b82c30b
Dechon Ryan <dechon@tuta.io>
2021-08-20 14:00:12 -0700

    Create white logo to accommodate red navbar bg.

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -4,7 +4,8 @@ import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton/LogoutButton';
 import { useSelector } from 'react-redux';
 
-import logo from '../../assets/images/forme-logo.png';
+import logoRed from '../../assets/images/forme-logo.png';
+import logoWhite from '../../assets/images/forme-logo-white.png';
 import './NavBar.css';
 
 const NavBar = () => {
@@ -13,27 +14,36 @@ const NavBar = () => {
   return (
     <nav className="navbar-container">
       <NavLink to='/' exact={true} activeClassName='active' className="navbar-links-home">
-        <img className="navbar-logo" src={logo} alt="\A" />
+        {/* <img className="navbar-logo" src={logoRed} alt="\A" /> */}
+        <img className="navbar-logo" src={logoWhite} alt="\A" />
       </NavLink>
       <ul className="navbar-links">
         <li>
           <NavLink to='/users' exact={true} activeClassName='active' className="navbar-link">
             Users
           </NavLink>
+          {user ? (
+            <NavLink to="/forms" activeClassName='active' className="navbar-link">
+              Forms
+            </NavLink>
+          ) : (
+            <>
+              <NavLink to='/sign-up' exact={true} className="navbar-auth-links-signup">
+                <span className="navbar-signup-text">
+                  Sign Up
+                </span>
+              </NavLink>
+
+              <NavLink to='/login' exact={true} className="navbar-auth-links-login">
+                <span className="navbar-login-text" onMouseEnter={(e) => e.target.innerText = "RAWRR!"} onMouseLeave={(e) => e.target.innerText = "Login"}>
+                  Login
+                </span>
+              </NavLink>
+            </>
+          )
+          }
         </li>
       </ul>
-      {/* <ul className="navbar-links">
-        <li>
-        {user !== null ? (
-            <NavLink to="/forms" activeClassName='active' className="navbar-link">
-            Forms
-          </NavLink>
-          )
-            :
-            null
-        }
-        </li>
-      </ul> */}
       <ul className="navbar-auth-links">
         <li>
           {user === null ?
