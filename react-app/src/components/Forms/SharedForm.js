50c79942ecfa25e2689d1bd2675cff6498487d8a
NoahNim <nmedoff123@gmail.com>
2021-08-22 14:13:18 -0700

    did some cleanup of used code

--- react-app/src/components/Forms/SharedForm.js
+++ react-app/src/components/Forms/SharedForm.js
@@ -1,37 +1,16 @@
 import React, { useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import {
-    createTextInput,
-    createMultiLineText,
-    createSelectField,
-    createMultipleChoice,
-    createCheckboxField,
-    createNumericInput,
-} from "@kagenlh/jsxfields";
 import { getSharedForm } from '../../store/forms.js'
 import './Forms.css'
 import FormField from './FormField.js';
-
-// Do we actually need this component? -DR
 
 function SharedForm() {
     const dispatch = useDispatch()
     const form = useSelector(state => state.forms)
     const { formId } = useParams()
-    // const [fieldsArray, _setFieldsArray ] = useState(form[formId]?.fields)
-    const [inputValue, setInputValue] = useState('')
-    const [multiLineTextValue, setMultiLineTextValue] = useState('')
-    const [selectFieldValue, setSelectFieldValue] = useState('')
-    const [multipleChoiceValue, setMultipleChoiceValue] = useState('')
-    const [checkboxFieldValue, setCheckboxFieldValue] = useState('')
-    const [numericInputValue, setNumericInputValue] = useState('')
 
     let fieldsArray = form[formId]?.fields  
-
-    // console.log('THIS IS THE FIELD ARRAY I HOPE!!!!!', fieldsArray)
-
-    // console.log('THIS IS THE FORM STATEf', form[formId]?.title)
 
     useEffect(() => {
         dispatch(getSharedForm(formId))
