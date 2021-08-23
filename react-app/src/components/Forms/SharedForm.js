905d48f200d220a235730620be95a810bb53540d
Kagen Hearn <kl.hearn40@gmail.com>
2021-08-23 09:55:35 -0400

    Made some last second styling fixes

--- react-app/src/components/Forms/SharedForm.js
+++ react-app/src/components/Forms/SharedForm.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams, Link } from 'react-router-dom'
 import { getSharedForm } from '../../store/forms.js'
-import './Forms.css'
+
+import redLogo from '../../assets/images/forme-logo.png';
+import './Forms.css';
 import FormField from './FormField.js';
 
 function SharedForm() {
@@ -17,15 +19,18 @@ function SharedForm() {
     }, [dispatch, formId])
 
     return (
-        <div>
-            <form classNames="shared-form">
-                <h2 className="form-title">{form[formId]?.title}</h2>
-                <p className='form-description'>{form[formId]?.description}</p>
+        <div className="shared-form-container">
+            <div className="shared-form-logo-container">
+                <img className="shared-form-logo" src={redLogo} alt="\A"/>
+            </div>
+            <form className="shared-form">
+                <h2 className="form-title" style={{display: "flex", justifyContent: form[formId]?.title_align}}>{form[formId]?.title}</h2>
+                <p className='form-description' style={{display: "flex", justifyContent: form[formId]?.description_align}}>{form[formId]?.description}</p>
                 {
                     fieldsArray?.map(field => <FormField field={field} />)
                 }
-                <Link to="/forms"><button className="forms-return">Back To Forms</button></Link>
             </form>
+            <Link to="/forms"><button className="forms-return">Submit</button></Link>
         </div>
 
     )
