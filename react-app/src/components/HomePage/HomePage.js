dc830fb4abeb5bb980c28f06fe6c3a034aed30af
Kagen Hearn <kl.hearn40@gmail.com>
2021-09-21 09:14:30 -0400

    Made splash page accessible only if logged in and reduced homepage navbar size back down

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,4 +1,5 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./HomePage.css";
 import fillForm from "../../assets/images/fill-in-form.png";
@@ -6,6 +7,14 @@ import dinoForm from "../../assets/images/wufoo-online-form-builder.png";
 import NavBar from "../NavBar/NavBar";
 
 export default function HomePage() {
+	const user = useSelector(state => state.session.user);
+
+	if(user) {
+		return (
+			<Redirect to="/forms"/>
+		);
+	}
+	
 	return (
 		<>
 			<NavBar />
