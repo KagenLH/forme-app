7bc18a1373a2714f74f578c656f10e57709abeca
Mark Vasquez <markjasonvasquez1@gmail.com>
2021-10-04 15:40:33 -0700

    fix shared link

--- react-app/src/components/Forms/ShareFormLinkModal.js
+++ react-app/src/components/Forms/ShareFormLinkModal.js
@@ -1,21 +1,28 @@
-import React, {useState} from 'react'
-import { Modal } from '../../context/Modal.js'
-import ShareFormLink from './ShareFormLink';
+import React, { useState } from "react";
+import { Modal } from "../../context/Modal.js";
+import ShareFormLink from "./ShareFormLink";
 
 function ShareFormLinkModal({ formId }) {
-    const [showModal, setShowModal] = useState(false);
-    const [shareFormLink, setShareFormLink] = useState(`http://www.forme-live.herokuapp.com/forms/${formId}/shared`)
+	const [showModal, setShowModal] = useState(false);
+	const [shareFormLink, setShareFormLink] = useState(
+		`http://forme-live.herokuapp.com/forms/${formId}/shared`
+	);
 
-    return (
-        <>
-            <i onClick={() => setShowModal(true)} className="fa fa-share-alt-square" title='Share' aria-hidden="true" />
-            {showModal && (
-                <Modal onClose={() => setShowModal(false)}>
-                    <ShareFormLink formUrl={shareFormLink} />
-                </Modal>
-            )}
-        </>
-    )
+	return (
+		<>
+			<i
+				onClick={() => setShowModal(true)}
+				className="fa fa-share-alt-square"
+				title="Share"
+				aria-hidden="true"
+			/>
+			{showModal && (
+				<Modal onClose={() => setShowModal(false)}>
+					<ShareFormLink formUrl={shareFormLink} />
+				</Modal>
+			)}
+		</>
+	);
 }
 
-export default ShareFormLinkModal
+export default ShareFormLinkModal;
